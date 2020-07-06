f939589437c98ef1fb9238812c982e1bdc153f68
Anna Kova <m3ta007@gmail.com>
2020-07-05 23:26:14 -0400

    Composition is added for Disclosure component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import './App.css'
+import './components/Disclosure.css'
+import './components/DisclosureMore.css'
 import Disclosure from './components/Disclosure'
 
 function App() {
   return (
     <div>
-      <Disclosure />
-
-      {/* </Disclosure> */}
+      <Disclosure
+        title="Disclosure Design Pattern"
+        trigger="Keyboard Interaction"
+        contentTop={
+          <p>
+            A{' '}
+            <a href="https://www.w3.org/TR/wai-aria-practices/#disclosure">
+              disclosure
+            </a>{' '}
+            is a button that controls visibility of a section of content. When
+            the controlled content is hidden, it is often styled as a typical
+            push button with a right-pointing arrow or triangle to hint that
+            activating the button will display additional content. When the
+            content is visible, the arrow or triangle typically points down.
+          </p>
+        }>
+        <p>When the disclosure control has focus: </p>
+        <ul>
+          <li>
+            <strong>Enter: </strong>
+            activates the disclosure control and toggles the visibility of the
+            disclosure content.
+          </li>
+          <li>
+            <strong>Space: </strong>
+            activates the disclosure control and toggles the visibility of the
+            disclosure content.
+          </li>
+        </ul>
+      </Disclosure>
     </div>
   )
 }
--- src/components/Disclosure.jsx
+++ src/components/Disclosure.jsx
@@ -10,43 +10,21 @@ function Disclosure(props) {
 
   return (
     <div className="disclosure-container">
-      <h1>Disclosure Design Pattern</h1>
-      <p>
-        A{' '}
-        <a href="https://www.w3.org/TR/wai-aria-practices/#disclosure">
-          disclosure
-        </a>{' '}
-        is a button that controls visibility of a section of content. When the
-        controlled content is hidden, it is often styled as a typical push
-        button with a right-pointing arrow or triangle to hint that activating
-        the button will display additional content. When the content is visible,
-        the arrow or triangle typically points down.
-      </p>
+      <h1>{props.title}</h1>
+      {props.contentTop}
       <button
         className="disclosure-btn"
         aria-expanded={isExpanded}
         aria-controls="disclosure"
         onClick={() => handleExpanded()}>
-        Keyboard Interaction
+        {props.trigger}
       </button>
       <div
         id="disclosure"
         className={`disclosure-content ${
           isExpanded ? 'disclosure-expanded' : 'disclosure-collapsed'
         }`}>
-        <p>When the disclosure control has focus: </p>
-        <ul>
-          <li>
-            <strong>Enter: </strong>
-            activates the disclosure control and toggles the visibility of the
-            disclosure content.
-          </li>
-          <li>
-            <strong>Space: </strong>
-            activates the disclosure control and toggles the visibility of the
-            disclosure content.
-          </li>
-        </ul>
+        {props.children}
       </div>
     </div>
   )
